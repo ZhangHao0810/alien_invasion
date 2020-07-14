1d2c2c86bb67a1aa0382ab2482a14d5d14e7160e
ZhangHao0810 <874993952@qq.com>
2020-07-14 19:52:27 +0800

    屏幕中加入飞船类, 第一次使用对象.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,27 +1,28 @@
 # sys 模块用于退出
 import sys
-
 import pygame
+
+from settings import Settings
+from ship import Ship
 
 def run_game():
     #初始化 游戏模块并且创建一个屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    #设置背景色
-    bg_color = (230,230,230)
+    #创建一艘飞船
+    ship = Ship(screen)
 
-    # 游戏主循环
+    # 开始游戏主循环
     while True:
+        #设置背景色, 重绘屏幕.
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
-        #监视鼠标和键盘事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        #背景色填充.
-        screen.fill(bg_color)
+    # #设置背景色
+    # bg_color = (230,230,230)
 
         # 让最近的屏幕可见  用于刷新
         pygame.display.flip()
